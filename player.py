b2eddfe6d655b9a823a0c62670945e61f38d49bb
Shayan Yousefian <shayan.yousefian1372@gmail.com>
2017-03-26 21:52:03 +0430

    Heuristic function added

--- player.py
+++ player.py
@@ -44,7 +44,42 @@ class Board:
                     if i == 99:
                         self.goal_state[0].append(ball)
 
-test_board = Board(
+    def heuristic_calc(self, user_state, goal_state=None):
+        if goal_state is None:
+            goal_state = list(self.goal_state)
+        state = list(user_state)
+        goal_state_tube_counts = {}
+        score = 0
+        index = 0
+        for tube in goal_state:
+            goal_state_tube_counts[len(tube)] = index
+            index += 1
+        print("=== testing state:")
+        self.display_tubes(state)
+        for key in reversed(sorted(goal_state_tube_counts.iterkeys())):
+            goal_tube = goal_state[goal_state_tube_counts[key]]
+            print("testing goal_tube:" + str(goal_state_tube_counts[key]))
+            print(goal_tube)
+            best_match_state_tube_index = None
+            best_match_state_tube_score = 0
+            state_tube_index = 0
+            for state_tube in state:
+                state_tube_score = 0
+                for i in range(0, min(len(state_tube), len(goal_tube))):
+                    if state_tube[i] == goal_tube[i]:
+                        state_tube_score += 1
+                if state_tube_score > best_match_state_tube_score:
+                    best_match_state_tube_index = state_tube_index
+                    best_match_state_tube_score = state_tube_score
+                state_tube_index += 1
+            if best_match_state_tube_index is not None:
+                del state[best_match_state_tube_index]
+                score += best_match_state_tube_score
+        print("score= " + str(score))
+        return 6 - score
+
+
+board = Board(
     goal_state=[
         ['G'],
         ['R', 'P'],
@@ -54,7 +89,12 @@ test_board = Board(
 
 print()
 print('=== tubes ===')
-test_board.display_tubes()
+board.display_tubes()
 print()
 print('=== goal state ===')
-test_board.display_tubes(test_board.goal_state)
+board.display_tubes(board.goal_state)
+board.heuristic_calc([
+    ['R', 'P'],
+    ['P', 'R', 'G'],
+    ['G']
+])
